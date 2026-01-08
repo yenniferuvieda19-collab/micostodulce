e88d0d2c522c7ede85d4a24371a30cd18277bc50
Andres26030 <garciaandrese2603@gmail.com>
2026-01-07 20:04:16 -0400

    Mis cambios locales antes de actualizar

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -7,9 +7,5 @@ class UsuarioModel extends Model{
       protected $table      = 'usuarios';
     protected $allowedFields = ['Nombre', 'Correo', 'Contraseña'];
     // Uncomment below if you want add primary key
-<<<<<<< HEAD
-    protected $primaryKey = 'Id_usuario';
-=======
     protected $primaryKey = 'id';
->>>>>>> 10d6151b1c435ca1dba5e99bf628a48ae3aff430
 }
