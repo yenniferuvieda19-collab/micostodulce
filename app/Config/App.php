87ff08a015c28a401c09169747119e3c3f5b79bb
Yennifer Pérez <yenniferuvieda19@gmail.com>
2026-01-11 21:50:07 -0400

    Vista de resetpasswor

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/micostodulce/public/';
+    public string $baseURL = 'http://localhost/mi-costo-dulce/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
